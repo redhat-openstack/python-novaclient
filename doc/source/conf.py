9b6037d02745342eb68b564f61c34bd908957ca3
Jakub Ruzicka <jruzicka@redhat.com>
2014-08-13 16:02:22 +0200

    Use oslo.sphinx instead of oslosphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -88,7 +88,7 @@ gen_ref("v3", "Version 3 API Reference",
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'oslosphinx']
+extensions = ['sphinx.ext.autodoc', 'oslo.sphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
